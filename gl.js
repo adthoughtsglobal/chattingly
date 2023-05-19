dd126bd4d7ba2688c28279ced7ce73e8703caaa2
adthoughtsglobal <123654929+adthoughtsglobal@users.noreply.github.com>
2023-05-19 13:46:36 +0530

    1.3

--- gl.js
+++ gl.js
@@ -8,7 +8,8 @@ elements.forEach((element) => {
 	localStorage.setItem("ThCook", "#3498db")
 	localStorage.setItem("ThCook2", "#00708a")
 	var elements = document.querySelectorAll('body *');
-elements.forEach((element) => {
-  element.style.setProperty('--main-color', localStorage.getItem("ThCook"));
-	element.style.setProperty('--dark-color', localStorage.getItem("ThCook2"));
+	elements.forEach((element) => {
+ 		element.style.setProperty('--main-color', localStorage.getItem("ThCook"));
+		element.style.setProperty('--dark-color', localStorage.getItem("ThCook2"));
+	});
 }
